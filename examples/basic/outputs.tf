eb44e0af9623340f8ed05a6954e2ff6c7c1265e0
Todd Giguere <todd.giguere@ibm.com>
2025-10-02 09:02:56 -0400

    fix outputs

--- examples/basic/outputs.tf
+++ examples/basic/outputs.tf
@@ -2,12 +2,12 @@
 # Outputs
 ##############################################################################
 
-output "ssh_key_id" {
-  description = "The ID of the ssh key"
-  value       = module.mock_module.ssh_key_id
-}
+# output "ssh_key_id" {
+#   description = "The ID of the ssh key"
+#   value       = module.mock_module.ssh_key_id
+# }
 
-output "fingerprint" {
-  description = "SSH key Fingerprint info"
-  value       = module.mock_module.fingerprint
-}
+# output "fingerprint" {
+#   description = "SSH key Fingerprint info"
+#   value       = module.mock_module.fingerprint
+# }
